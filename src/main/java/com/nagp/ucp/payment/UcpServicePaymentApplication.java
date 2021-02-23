b1aa3ceae6b7436616487e8798630250e33f7128
superuserz <manmeet.devgun@yahoo.in>
2021-02-23 16:52:42 +0530

    updated

--- src/main/java/com/nagp/ucp/payment/UcpServicePaymentApplication.java
+++ src/main/java/com/nagp/ucp/payment/UcpServicePaymentApplication.java
@@ -3,16 +3,15 @@ package com.nagp.ucp.payment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableSwagger2
+@ComponentScan({ "com.nagp.ucp" })
 public class UcpServicePaymentApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(UcpServicePaymentApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(UcpServicePaymentApplication.class, args);
+	}
 
 }
