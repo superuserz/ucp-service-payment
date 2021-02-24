ce5295282f37802412815bbdad3404dbf8ae748c
superuserz <manmeet.devgun@yahoo.in>
2021-02-24 23:43:22 +0530

    updated

--- src/main/java/com/nagp/ucp/payment/UcpServicePaymentApplication.java
+++ src/main/java/com/nagp/ucp/payment/UcpServicePaymentApplication.java
@@ -1,8 +1,10 @@
 package com.nagp.ucp.payment;
 
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
@@ -12,6 +14,12 @@ public class UcpServicePaymentApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UcpServicePaymentApplication.class, args);
+	}
+
+	// Configured For Rabbit Listener
+	@Bean
+	public org.springframework.amqp.support.converter.MessageConverter converter() {
+		return new Jackson2JsonMessageConverter();
 	}
 
 }
--- src/main/java/com/nagp/ucp/payment/controller/PaymentController.java
+++ src/main/java/com/nagp/ucp/payment/controller/PaymentController.java
@@ -2,9 +2,16 @@ package com.nagp.ucp.payment.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.nagp.ucp.common.constant.QueueConstants;
+import com.nagp.ucp.common.exception.UCPException;
+import com.nagp.ucp.common.request.NotificationPayload;
+import com.nagp.ucp.common.responses.BaseResponse;
 
 @RestController
 @RequestMapping(value = "/payment")
@@ -12,8 +19,18 @@ public class PaymentController {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(PaymentController.class);
 
-	@GetMapping(value = "/greet")
-	public String greet() {
-		return "Hi From Payments";
+	@Autowired
+	private RabbitTemplate publisher;
+
+	@PostMapping(value = "/doPayment")
+	public BaseResponse<String> doPayment() throws UCPException {
+
+		// process payment logic
+		LOGGER.info("Making Payment");
+		String message = "Your Payment Has been processed.";
+		String subject = "UCP Payment";
+		NotificationPayload payload = new NotificationPayload(message, subject, null, null);
+		publisher.convertAndSend(QueueConstants.NOTIFICATION_EXCHANGE, QueueConstants.NOTIFICATION_ROUTING, payload);
+		return new BaseResponse<>("Your Payment is Successful");
 	}
 }
